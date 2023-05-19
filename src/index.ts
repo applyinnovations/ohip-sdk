27d2b9bcad9fd0e7d1b879c005fcaef07bcbe350
airon-applyinnovations <airon@applyinnovations.com.au>
2023-05-19 16:39:51 +0800

    fix log

--- src/index.ts
+++ src/index.ts
@@ -104,11 +104,10 @@ export class Api {
         throw error;
       }
 
-      // retry auth
-      this.authTries += 1;
       console.warn(
         `Requesting new OHIP session token failed using credentials[${this.activeCredentialIndex}], retrying ${this.authTries}/${this.authRetriesLimit}`,
       );
+      this.authTries += 1;
       await this.requestNewAuthToken();
     }
   }
