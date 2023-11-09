ea7143dfe1d277c6102ddd7bc4dbac1d694dc753
applyinnovations <alexander@applyinnovations.com.au>
2023-11-09 14:00:47 +1000

    updated comments in patch docs to include new step

--- patch-docs.sh
+++ patch-docs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# replace all date-time with string pattern instead as datetime format from OHIP is non-standard
 grep -rl '"format": "date-time"' hospitality-api-docs | while read -r file; do
   if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS requires an explicit argument for -i, here using an empty string ('')
@@ -9,3 +10,5 @@ grep -rl '"format": "date-time"' hospitality-api-docs | while read -r file; do
     sed -i 's|"format": "date-time"|"pattern": "/([0-9]{4})-(?:[0-9]{2})-([0-9]{2}) ([0-9]{2}):([0-9]{2}):([0-9]{2}).([0-9]+)/"|g' "$file"
   fi
 done
+
+# remove all "body" parameters from requests using jq
