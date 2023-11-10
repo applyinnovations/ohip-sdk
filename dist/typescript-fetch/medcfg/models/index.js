b15645e2402b85d04f2a6e4b498923acdb2af95a
applyinnovations <alexander@applyinnovations.com.au>
2023-11-10 17:22:07 +1000

    fixed mandetory host ts error

---
+++ dist/typescript-fetch/medcfg/models/index.js
@@ -0,0 +1,56 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* tslint:disable */
+/* eslint-disable */
+__exportStar(require("./CustomizedLetter"), exports);
+__exportStar(require("./CustomizedLetterStatus"), exports);
+__exportStar(require("./CustomizedLetterType"), exports);
+__exportStar(require("./EmailFile"), exports);
+__exportStar(require("./EmailFileType"), exports);
+__exportStar(require("./EmailFolioReportRequest"), exports);
+__exportStar(require("./EmailRecipientType"), exports);
+__exportStar(require("./EmailRecipientsType"), exports);
+__exportStar(require("./ErrorInstance"), exports);
+__exportStar(require("./ExceptionDetailType"), exports);
+__exportStar(require("./FileAttachmentType"), exports);
+__exportStar(require("./FileToUpload"), exports);
+__exportStar(require("./FolioReport"), exports);
+__exportStar(require("./FolioReportCriteriaType"), exports);
+__exportStar(require("./FolioReportResultType"), exports);
+__exportStar(require("./FolioReportToEmail"), exports);
+__exportStar(require("./Image"), exports);
+__exportStar(require("./ImageCodeType"), exports);
+__exportStar(require("./ImageParameterType"), exports);
+__exportStar(require("./ImageParametersType"), exports);
+__exportStar(require("./ImageResultType"), exports);
+__exportStar(require("./ImageResultsType"), exports);
+__exportStar(require("./ImageStyleType"), exports);
+__exportStar(require("./ImageToUpload"), exports);
+__exportStar(require("./ImageUploadInfoType"), exports);
+__exportStar(require("./ImageUploadType"), exports);
+__exportStar(require("./InstanceLink"), exports);
+__exportStar(require("./Links"), exports);
+__exportStar(require("./ModelFile"), exports);
+__exportStar(require("./RegistrationCard"), exports);
+__exportStar(require("./RegistrationCardType"), exports);
+__exportStar(require("./SetCustomizedLetterRequest"), exports);
+__exportStar(require("./Status"), exports);
+__exportStar(require("./UniqueIDType"), exports);
+__exportStar(require("./UploadFileAttachmentRequest"), exports);
+__exportStar(require("./UploadImageRequest"), exports);
+__exportStar(require("./WarningType"), exports);
+__exportStar(require("./WarningsType"), exports);
---
+++ dist/typescript-fetch/parext/models/index.js
@@ -0,0 +1,47 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* tslint:disable */
+/* eslint-disable */
+__exportStar(require("./CodeDescriptionType"), exports);
+__exportStar(require("./CorporateRatesSearchType"), exports);
+__exportStar(require("./ErrorInstance"), exports);
+__exportStar(require("./ExceptionDetailType"), exports);
+__exportStar(require("./HotelAvailabilityStatus"), exports);
+__exportStar(require("./InstanceLink"), exports);
+__exportStar(require("./Links"), exports);
+__exportStar(require("./MultiRoomRateAvailabilityRequestType"), exports);
+__exportStar(require("./MultiRoomRateAvailabilityResponseType"), exports);
+__exportStar(require("./MultiRoomRateAvailabilityType"), exports);
+__exportStar(require("./MultiRoomRateStayType"), exports);
+__exportStar(require("./MultiRoomRateType"), exports);
+__exportStar(require("./MultiRoomTypesType"), exports);
+__exportStar(require("./OfferTotalType"), exports);
+__exportStar(require("./PostMultiRoomRateAvailabilityRequest"), exports);
+__exportStar(require("./PropertySearchPropertyInfo"), exports);
+__exportStar(require("./RatesSearchType"), exports);
+__exportStar(require("./RoomRateInfoType"), exports);
+__exportStar(require("./RoomRatePriceInfoType"), exports);
+__exportStar(require("./RoomTagType"), exports);
+__exportStar(require("./RoomsSearchType"), exports);
+__exportStar(require("./SearchPropertyRequestType"), exports);
+__exportStar(require("./SearchPropertyResponseType"), exports);
+__exportStar(require("./SearchPropertyRoomStayType"), exports);
+__exportStar(require("./SearchRoomClassificationType"), exports);
+__exportStar(require("./ShopBaseRequest"), exports);
+__exportStar(require("./SortByType"), exports);
+__exportStar(require("./WarningType"), exports);
+__exportStar(require("./WarningsType"), exports);
